d4a0b81e31c29fc46f5747a76c48fd0d5dc7922b
vivian-rogers <43078448+vivian-rogers@users.noreply.github.com>
2021-06-22 14:23:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ printf "Need QE 6.4.1 with Sc Hubbard +U? Enter 1 or 0 for yes or no: "
 read booltest
 if (($booltest == 1 )); then
 	cd ./install
-	bash build641_xconfigure	
+	bash build641_xconfigure > install.log	
 	cd q-e-6.4.1/bin
 	addPrefixPath qeEditedPath
 	cd ../../	
